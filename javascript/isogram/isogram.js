2a0dc4741920e31852c64b8fe88aa38aecb79641
Russell Osborne <russell@burningpony.com>
2016-05-21 18:18:06 -0400

    Set is a thing in JS

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,23 +1,10 @@
-Array.prototype.unique = function() {
-  'use strict';
-
-  var n = {},
-    r = [];
-  for (var i = 0; i < this.length; i++) {
-    if (!n[this[i]]) {
-      n[this[i]] = true;
-      r.push(this[i]);
-    }
-  }
-  return r;
-}
-
-
 module.exports = function(str) {
   'use strict';
 
   this.isIsogram = function() {
     var chars = str.toLowerCase().match(/\w/g);
-    return chars.length === chars.unique().length;
+    var charSet = new Set(chars);
+
+    return chars.length === charSet.size;
   };
 };
